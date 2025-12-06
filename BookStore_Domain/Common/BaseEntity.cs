54f2c07f8446f76b5b2f772a646f5430fd6745b1
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2025-12-06 11:02:35 +0330

    Add BaseEntity and Book entity structure

---
+++ BookStore_Domain/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Domain.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }= Guid.NewGuid();
+        public DateTime CreateAt { get; set; }=DateTime.UtcNow;
+        public DateTime? UpdateAt { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ BookStore_Domain/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Common;
+
+namespace BookStore.Domain.Entities
+{
+    public class Book:BaseEntity
+    {
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Slog { get; set; }
+    }
+}
