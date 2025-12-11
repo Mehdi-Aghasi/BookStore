6099bf5aad2fca80189ac2a456de0ab12bdc418f
Mehdi Aghasi <mahdi.aghasiwaz@gmail.com>
2025-12-11 18:36:03 +0330

    add Dto for Category Model

---
+++ BookStore_Application/DTOs/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Application.DTOs
+{
+    public class CategoryDto
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Slug { get; set; }
+    }
+}
---
+++ BookStore_Application/DTOs/UpdateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Application.DTOs
+{
+    public class UpdateCategoryDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
